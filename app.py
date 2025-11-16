12550891c20946f6d3f19cfb30818a31a70e44d9
Wilyam390 <wyammine.ieu2023@student.ie.edu>
2025-11-16 21:59:59 +0100

    Basic UI

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Flask is working.</h1><p>great</p>"
+    """Display homepage with tasks"""
+    return render_template('index.html')
 
 @app.route('/health')
 def health():
